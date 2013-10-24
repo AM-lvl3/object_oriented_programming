97d9ee17da317e22c78fdab599af17384ad67534
AM-lvl3 <drewmorris@live.com>
2013-10-24 15:15:19 -0400

    added name variable

--- people.rb
+++ people.rb
@@ -1,15 +1,41 @@
-class Student
+class Person
+
+	def initialize(name)
+		@name = name
+		
+	end
+
+	def say_greeting
+		greeting = "Hi, my name is "
+	end
+
+end
+
+class Student < Person
 
 	def learn
 		"I get it!"
 	end
 
-end
-
-class Instructor
-
-	def teach
-
+	def say_greeting
+		super + @name + "!"
 	end
 
 end
+
+class Instructor < Person
+
+	def teach
+		"Everything in Ruby is an Object."
+	end
+
+	def say_greeting
+		super
+	end
+
+end
+
+person = Person.new("name")
+student = Student.new("Cristina")
+puts student.say_greeting
+
